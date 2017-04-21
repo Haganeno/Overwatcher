7f312cf7388c71bfc808ad02bac72cd8f7072a44
Uwhipa <macfly.lkn@free.fr>
2017-04-21 10:21:49 +0200

    Parser terminé

--- parser_json.php
+++ parser_json.php
@@ -8,46 +8,55 @@ class Parser {
   	$overall_stats = array();
   	$game_stats = array();
   	$average_stats = array();
-  	$hero_stats = array();
+  	$competitive_hero_stats = array();
+  	$quickplay_hero_stats = array();
   	$playtime = array();
   	$achievements = array();
   	foreach($json_data as $account => $value){
-  		if($account != "_request" && $account != "any"){
+  		if($account != "_request" && $account != "any" && $value!=null){
   			$account_list[$account] = true;
-  			foreach($value as $request = $val){
+  			foreach($value as $request => $val){
   				if($request == "stats"){
   					foreach($val as $mode => $res){
   						foreach($res as $type => $data){
-  							foreach($data as $title => $number){
-  								switch($type){
-  								case "overall_stats":
-  									$overall_stats[$mode][$title]=$number;
-  									break;
-  								case "game_stats":
-  									$game_stats[$mode][$title]=$number;
-  									break;
-  								case "average_stats":
-  									$average_stats[$mode][$title]=$number;
-  									break;
-  								case:"competitive":
-  									break;
+  							if($type!="competitive"){
+  								foreach($data as $title => $number){
+  									switch($type){
+  									case "overall_stats":
+  										$overall_stats[$mode][$title]=$number;
+  										break;
+  									case "game_stats":
+  										$game_stats[$mode][$title]=$number;
+  										break;
+  									case "average_stats":
+  										$average_stats[$mode][$title]=$number;
+  										break;
+  									}
   								}
   							}
   						}
   					}
   				}
-  				else if ($request = "heroes"){
+  				else if ($request == "heroes"){
   					foreach($val as $stat_or_playtime => $res){
   						foreach($res as $mode => $name){
-  							if($stat_or_playtime == "stats"){
+  						$name_keys = array_keys($name);
+  						
+  						if($stat_or_playtime == "stats"){
   								foreach($name as $general_or_specific => $data){
+  								$compt = 0;
   									foreach($data as $title => $number){
-  										$hero_stats[$name][$mode]=$number;
+  										if($mode =="competitive"){
+  											$competitive_hero_stats[$name_keys[$compt]][$title]=$number;
+  										}else{
+  											$quickplay_hero_stats[$name_keys[$compt]][$title]=$number;
+  										}
+  										$compt++;
   									}
                    }
   							}else { //Playtime
   								foreach($name as $hero_data => $number){
-  									$playtime[$name][$mode] = $number;
+  									$playtime[$hero_data][$mode] = $number;
   								}
   							}
   						}
@@ -66,8 +75,11 @@ class Parser {
   	$return["accounts"] = $account_list;
   	$return["game_stats"] = $game_stats;
   	$return["average_stats"] = $average_stats;
-  	$return["heroes_stats"] = $hero_stats;
+  	$return["competitive_heroes_stats"] = $competitive_hero_stats;
+  	$return["quickplay_heroes_stats"] = $quickplay_hero_stats;
   	$return["playtime"] = $playtime;
   	$return["achievement_list"] = $achievements;
   	return $return;
   }
+}
+?>
